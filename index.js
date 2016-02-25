4586e2039c82070e320b8200fa23ed9255813f79
Romain Giovanetti <ganoninc@gmail.com>
2016-02-25 14:47:32 +0100

    Accents support in hashtags

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ angular.module('twitterFilters', ['ngSanitize'])
 		return function(text) {
             var urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 			var twitterUserRegex = /@([a-zA-Z0-9_]{1,20})/g;
-			var twitterHashTagRegex = /\B#(\w+)/g;
+			var twitterHashTagRegex = /\B#([a-z,A-Z,á,é,í,ó,ú,â,ê,ô,ã,õ,ç,Á,É,Í,Ó,Ú,Â,Ê,Ô,Ã,Õ,Ç,ü,ñ,Ü,Ñ]+)/g;
 
 			text = text.replace(urlRegex,"<a href='$&' target='_blank'>$&</a>");
 			text = text.replace(twitterUserRegex,"<a href='http://www.twitter.com/$1' target='_blank'>@$1</a>");
