cdedc3eb1e5f13050cc53340326b3647ffb50dc5
Romain Giovanetti <ganoninc@gmail.com>
2016-03-15 16:00:53 +0100

    Corrected hashtags with numbers parsing

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 angular.module('twitterFilters', ['ngSanitize'])
 	.filter('linkUsername', function() {
 		return function(text) {
-			return '<a href="http://twitter.com/' + text.slice(1) + '">' + text + '</a>';
+			return '<a class="mention" href="http://twitter.com/' + text.slice(1) + '">' + text + '</a>';
 		};
 	})
 	.filter('linkHashtag', function() {
 		return function(text) {
-			return '<a href="http://twitter.com/search/%23' + text.slice(1) + '">' + text + '</a>';
+			return '<a class="hashtag" href="http://twitter.com/search/%23' + text.slice(1) + '">' + text + '</a>';
 		};
 	})
 	.filter('tweet', function() {
 		return function(text) {
             var urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 			var twitterUserRegex = /@([a-zA-Z0-9_]{1,20})/g;
-			var twitterHashTagRegex = /\B#([a-z,A-Z,á,é,í,ó,ú,â,ê,ô,ã,õ,ç,Á,É,Í,Ó,Ú,Â,Ê,Ô,Ã,Õ,Ç,ü,ñ,Ü,Ñ]+)/g;
+			var twitterHashTagRegex = /\B#([a-z,A-Z,á,é,í,ó,ú,â,ê,ô,ã,õ,ç,Á,É,Í,Ó,Ú,Â,Ê,Ô,Ã,Õ,Ç,ü,ñ,Ü,Ñ,0-9]+)/g;
 
             text = text.replace(urlRegex,"<a class=\"url\" href='$&' target='_blank'>$&</a>");
             text = text.replace(twitterUserRegex,"<a class=\"mention\" href='http://www.twitter.com/$1' target='_blank'>@$1</a>");
